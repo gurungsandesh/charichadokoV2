7a35ee2b5742ed4d3bd516968aff7e13c50fe487
singok <singoksuraj@gmail.com>
2022-06-08 22:51:56 +0545

    Category API Created

---
+++ Server/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category_name' => 'required | unique:categories'
+        ]);
+        $info = Category::create($request->all());
+        if ($info) {
+            return response(['success' => 'Category Added Successfully.'], 201);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'category_name' => 'required'
+        ]);
+        $info = Category::find($id)->update($request->all());
+        if ($info) {
+            return response(['success' => 'Category Updated Successfully'], 200);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $info = Category::find($id)->delete();
+        if ($info) {
+            return response(['success' => 'Category Deleted Successfully.'], 200);
+        }
+    }
+}
--- Server/routes/api.php
+++ Server/routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\CategoryController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*-------------Category (public)-----------------*/
+Route::controller(CategoryController::class)->group(function () {
+    Route::post('category', 'store');           // add category
+    Route::get('category/{id}', 'show');        // search category 
+    Route::get('category', 'index');            // show all category
+    Route::delete('category/{id}', 'destroy');  // delete category
+    Route::put('category/{id}', 'update');      // update category
+});
+/*----------------End-------------------*/
